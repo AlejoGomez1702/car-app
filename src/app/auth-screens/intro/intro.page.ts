98f30419351d86a8c97f69bb0cb1a644a1616b04
Alejandro Gómez <alejandrogomez2339@gmail.com>
2025-01-17 17:47:18 -0500

    Auth page

--- src/app/auth-screens/intro/intro.page.ts
+++ src/app/auth-screens/intro/intro.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { register } from 'swiper/element/bundle';
+register();
+
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.page.html',
