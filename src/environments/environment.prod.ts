f7ac307df1ed0b2f964707fb85c124b0109ba575
Alejandro Gómez <alejandrogomez2339@gmail.com>
2025-01-17 18:13:38 -0500

    congig firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBfGHy9F0U8N664ZV5l2cFsEohlbAm9kjk",
+    authDomain: "car-app-bd3a8.firebaseapp.com",
+    projectId: "car-app-bd3a8",
+    storageBucket: "car-app-bd3a8.firebasestorage.app",
+    messagingSenderId: "989430638300",
+    appId: "1:989430638300:web:040b72ae9a4784702ab619",
+    measurementId: "G-LYXJET5BDG"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBfGHy9F0U8N664ZV5l2cFsEohlbAm9kjk",
+    authDomain: "car-app-bd3a8.firebaseapp.com",
+    projectId: "car-app-bd3a8",
+    storageBucket: "car-app-bd3a8.firebasestorage.app",
+    messagingSenderId: "989430638300",
+    appId: "1:989430638300:web:040b72ae9a4784702ab619",
+    measurementId: "G-LYXJET5BDG"
+  }
 };
 
 /*
